f71f4a2cc5b5ae1e374c065587dab7f09ddf5348
ElyPoli <itsos.poli@gmail.com>
2023-09-26 17:35:56 +0200

    create model, controller, print basic movies card

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,22 @@
-@extends("layouts.public")
+@extends('layouts.public')
 
-@section("content")
-    <h1>Hello world</h1>
+@section('content')
+    <div class="container">
+        <h1>Movies</h1>
+        <div class="row row-cols-4">
+            @foreach ($moviesList as $movie)
+                <div class="col">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $movie->title }}</h5>
+                            <p class="card-text"> {{ $movie->original_title }} </p>
+                            <p class="card-text"> {{ $movie->nationality }} </p>
+                            <p class="card-text"> {{ $movie->date }} </p>
+                            <p class="card-text"> {{ $movie->vote }} </p>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
 @endsection
--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -3,9 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Esercizio 55: Laravel - Model-Controller</title>
 
     @vite(["resources/js/app.js", "resources/scss/app.scss"])
+
+    <!-- Icona -->
+    <link rel="icon" href="{{ asset('img/icon-logo.png') }}">
 </head>
 <body>
     <main>
